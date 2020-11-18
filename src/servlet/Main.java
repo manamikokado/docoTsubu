1d09132c851a0ecf07e7184f8bd5093428ecdca9
mnmkkd <chine.mybestway.kim313@gmail.com>
2020-11-18 23:10:53 +0900

    add mutter Function

--- src/servlet/Main.java
+++ src/servlet/Main.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import model.Mutter;
+import model.PostMutterLogic;
 import model.User;
 
 /**
@@ -48,5 +49,34 @@ public class Main extends HttpServlet {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 			dispatcher.forward(request, response);
 		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエストパラメータを取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		// 入力値チェック
+		if(text != null && text.length() != 0) {
+			// アプリケーションスコープに保存されたつぶやきリストを取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+			// セッションスコープ内のユーザ情報を取得
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+			// つぶやきをリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+
+			// アプリケーションスコープにリストを保存
+			application.setAttribute("mutterList", mutterList);
+		}
+
+		// メイン画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
 	}
 }
